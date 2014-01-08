7b1efe6c0ef05645944d5f8d3cf681a626843dd1
Apprentice <apprentice@dbc03.local>
2014-01-08 15:33:17 -0800

    Adds solution to release 1, global linear search

--- source/linear_search.rb
+++ source/linear_search.rb
@@ -7,8 +7,21 @@ def linear_search(num, array)
   nil
 end
 
+def global_linear_search(to_find, array)
+  i = 0
+  matched_indices = []
+  while i < array.length
+    matched_indices << i if array[i] == to_find
+    i += 1
+  end
+  matched_indices
+end
+
 #-----DRIVERS-----
 random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ]
 p linear_search(18, random_numbers) == 2
 p linear_search(72, random_numbers) == 4
 p linear_search(20, random_numbers) == nil
+
+bananas_arr = "bananas".split(//)
+p global_linear_search("a", bananas_arr) == [1, 3, 5]
