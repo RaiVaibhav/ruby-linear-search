0511635a70e990f3b440ae82f8ff525f74032a26
Apprentice <apprentice@dbc04.local>
2014-01-08 16:04:04 -0800

    Add global_linear_search method which returns all the indices of matching objects in the array

--- source/linear_search.rb
+++ source/linear_search.rb
@@ -5,8 +5,21 @@ def linear_search(object, array)
   nil
 end
 
+def global_linear_search(object, array)
+  indices = []
+  for i in 0...array.length
+    indices << i if object == array[i]
+  end
+  indices
+end
+
 random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ]
 p linear_search(18, random_numbers) == 2
 # => 2
 p linear_search(9, random_numbers) == nil
 # => nil
+
+bananas_arr = "bananas".split(//)
+# => ["b", "a", "n", "a", "n", "a", "s"]
+p global_linear_search("a", bananas_arr)
+# => [ 1, 3, 5 ]
