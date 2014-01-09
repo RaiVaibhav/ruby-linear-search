9a682ff669ca3abc414b4f34a3deb0c3a9065cb9
Apprentice <apprentice@dbc02.local>
2014-01-08 18:25:47 -0800

    Completed release 1

--- source/linear_search.rb
+++ source/linear_search.rb
@@ -10,8 +10,20 @@ def linear_search(obj, array)
   p instances
 end
 
-random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ]
-linear_search(18, random_numbers)
-# => 2
-linear_search(9, random_numbers)
-# => nil
+def global_linear_search(obj, array)
+  counter = 0
+  instances = []
+  until array[counter] == nil
+    if obj == array[counter]
+      instances += [counter]
+    end
+    counter += 1
+  end
+  p instances
+end
+
+bananas_arr = "bananas".split(//)
+# => ["b", "a", "n", "a", "n", "a", "s"]
+
+
+p global_linear_search("a", bananas_arr) == [ 1, 3, 5 ]
