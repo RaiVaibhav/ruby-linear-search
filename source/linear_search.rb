a04569b697be27b0c3651531f32fc2eeec15509f
Apprentice <apprentice@dbc29.local>
2014-01-08 16:17:39 -0800

    Release 1: global linear search

--- source/linear_search.rb
+++ source/linear_search.rb
@@ -7,6 +7,16 @@ def linear_search(num, dict)
   nil
 end
 
+def global_linear_search(char, dict)
+  occurrences = []
+  index = 0
+  while index < dict.length
+    occurrences.push(index) if dict[index] == char
+    index += 1
+  end
+  occurrences
+end
+
 
 
 random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ]
@@ -14,4 +24,10 @@ p linear_search(18, random_numbers)
 # => 2
 p linear_search(9, random_numbers)
 # => nil
+
+
+bananas_arr = "bananas".split(//)
+# => ["b", "a", "n", "a", "n", "a", "s"]
+p global_linear_search("a", bananas_arr)
+# => [ 1, 3, 5 ]
 
