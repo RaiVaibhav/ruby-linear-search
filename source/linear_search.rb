868f97ef60aeadda4b221fee855a2515041fdf62
Ricardo Rubio <rickarubio@gmail.com>
2014-01-08 16:24:12 -0800

    global linear search implemented

--- source/linear_search.rb
+++ source/linear_search.rb
@@ -10,6 +10,21 @@ def linear_search(obj, array)
   nil
 end
 
+def global_linear_search(obj, array)
+  current_array_index = 0
+  matches = []
+  for i in array
+    matches << current_array_index if obj == i
+    current_array_index += 1
+  end
+  matches.empty? ? nil : matches
+end
+
+# Driver Code
 random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ]
 p linear_search(18, random_numbers) == 2
 p linear_search(9, random_numbers) == nil
+
+bananas_arr = "bananas".split(//)
+p global_linear_search("a", bananas_arr) == [ 1, 3, 5 ]
+p global_linear_search("z", bananas_arr) == nil
