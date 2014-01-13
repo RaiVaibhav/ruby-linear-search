b604772434eb82eade280242c354b5f114b17508
Emmanuel Kaunitz <ekaunitz@gmail.com>
2014-01-12 17:48:53 -0800

    global search done, each with index FTW, how can i re-factor.

--- source/linear_search.rb
+++ source/linear_search.rb
@@ -31,5 +31,23 @@ end
 
 
 random_numbers = [6, 29, 18, 2, 72, 19, 18, 10, 37]
-p linear_search(18, random_numbers) == 2 
-p linear_search(9, random_numbers) == nil
+ linear_search(18, random_numbers) == 2 
+ linear_search(9, random_numbers) == nil
+
+#release 1 
+def global_linear_search(find_me, source_array)
+global_search_array = []
+	source_array.each_with_index do |each_char,index|
+		if each_char == find_me
+			global_search_array << index
+		end
+	end
+ global_search_array
+end
+
+bananas_arr = "bananas".split(//)
+# => ["b", "a", "n", "a", "n", "a", "s"]
+p global_linear_search("a", bananas_arr) == [1,3,5]
+# => [ 1, 3, 5 ]
+
+
